fd2900e6831438967e8ee2fcdb0903a9f92fed08
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-02-08 11:18:16 +0530

    Add files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
         Point p1 =  new Point(4,9);
         Point p2 = new Point(3,6);
@@ -6,8 +7,12 @@ public class Main {
         Point p4 = new Point(12,36);
         Line l1 = new Line(p1,p2);
         Line l2 = new Line(p3,p4);
-        double diffe = Math.abs(l1.length()-l2.length());
 
-        System.out.println("Differnce between two lines is:"+diffe);
+            double diff = Math.abs(l1.length()-l2.length());
+        System.out.println("Difference of two lines are"+diff);
+
+
+
+
     }
 }
